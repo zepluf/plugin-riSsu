b1f2ef09b53262ab15fe93ca625a128079b73402
yellow1912 <yellow1912@gmail.com>
2012-06-28 02:30:47 +0700

    add spider option

--- lib/cores/Link.php
+++ lib/cores/Link.php
@@ -32,18 +32,18 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class Link {
 
-    private $pages = array(), $aliases = array();
-    
-  
+    private $pages = array(), $aliases = array(), $spider = false;
+
+
     public function __construct(){
-        $this->pages = Plugin::get('settings')->get('riSsu.pages');   
+        $this->pages = Plugin::get('settings')->get('riSsu.pages');
         $default = Plugin::get('settings')->get('riSsu.default');
 
         foreach($this->pages as $page => $options){
             if(is_array($this->pages[$page]))
                 $this->pages[$page] = array_merge($default, $this->pages[$page]);
-            else 
-                $this->pages[$page] = $default;  
+            else
+                $this->pages[$page] = $default;
             if(!empty($options['alias']) && $options['alias'] != $page)
                 $this->aliases[$options['alias']] = $page;
 
@@ -51,11 +51,25 @@ class Link {
             if(isset($options['identifier']) && !empty($options['identifier']))
                 Plugin::get('riSsu.'.$this->pages[$page]['parser'])->addIdentifier($page, $options['identifier']);
         }
+
+        // we need to know if a spider is visit us
+        if(!isset($_SESSION['spider_flag'])){
+            $_SESSION['spider_flag'] = false;
+            $spiders = Plugin::get('settings')->get('riSsu.spiders');
+            $user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+            foreach($spiders as $spider){
+                if(strpos($user_agent, $spider) !== false){
+                    $_SESSION['spider_flag'] = true;
+                    break;
+                }
+            }
+        }
+        $this->spider = $_SESSION['spider_flag'];
     }
-    
-    public function decode() {   
+
+    public function decode() {
         if(!Plugin::get('settings')->get('riSsu.status')) return false;
-        
+
         global $request_type;
         // do not do anything inside admin
         //if (ZMSettings::get('isAdmin')) return false;
@@ -64,15 +78,15 @@ class Link {
 
         // do not decode if this is in the excluded list
         if(isset($_GET['main_page'])){
-            
+
             // there are certain pages we need to assign new key to it
             $page = $_GET['main_page'];
             if($_GET['main_page'] == 'index' && isset($_GET['cPath']))
                 $page = 'categories';
-                
+
             if (!array_key_exists($page, Plugin::get('settings')->get('riSsu.pages'))) return false;
         }
-        
+
         // remove the catalog dir from the link
         $catalog_dir = ($request_type == 'NONSSL') ? DIR_WS_CATALOG : DIR_WS_HTTPS_CATALOG;
 
@@ -81,11 +95,11 @@ class Link {
         $request_uri = $_request_uri = Plugin::get('riUtility.Uri')->getCurrent();
         //$request_uri = rawurldecode($_request_uri);
         $is_dynamic_link = strpos($request_uri, 'index.php') !== false;
-        
+
         // we need to remove the extension first                        
         if(!$is_dynamic_link){
             $file_extension = pathinfo(parse_url($request_uri, PHP_URL_PATH), PATHINFO_EXTENSION);
-            if(!empty($file_extension)) {            
+            if(!empty($file_extension)) {
                 //$defined_extension = Plugin::get('settings')->get('riSsu.file_extension', '');
                 // hack for abc
                 $redirect_extension = Plugin::get('settings')->get('riSsu.redirect_extension', '');
@@ -97,7 +111,7 @@ class Link {
                     $response->send();
                     exit();
                 }
-                
+
                 // the extension matches what we expect, lets remove it from the link anyhow. It's just
                 // for show
                 if(!empty($defined_extension)){
@@ -105,26 +119,26 @@ class Link {
                 }
             }
         }
-        
+
         $original_uri = trim($catalog_dir=='/' ? $request_uri : preg_replace($regex,'', $request_uri, 1), '/');
 
         $first_param = current(explode('/', $original_uri, 2));
 
         // if the index.php is in the url, lets see if we need to rebuild the path and redirect.
         if($is_dynamic_link){
-            if(!isset($_GET['main_page']) || empty($_GET['main_page'])){  
+            if(!isset($_GET['main_page']) || empty($_GET['main_page'])){
                 // we can redirect to the shop url without the index.php
                 $this->redirect($this->getPageBase());
             }
-            else{             
-                if (($link = $this->link($_GET['main_page'], http_build_query($_GET), $request_type)) != false) {     
+            else{
+                if (($link = $this->link($_GET['main_page'], http_build_query($_GET), $request_type)) != false) {
                     $this->redirect($link);
                 }
                 // else we should redirect to a page not found
 
             }
         }
-		
+
         // if we are using multi-lang, then we should have language code at the very beginning
         if(Plugin::get('settings')->get('riSsu.multilang_status')){
             $languages_code = $first_param;
@@ -134,9 +148,9 @@ class Link {
             // if this is the default language, we may need redirection here
             if(Plugin::get('settings')->get('riSsu.default_language_status')){
                 if($languages_code == DEFAULT_LANGUAGE)
-                $redirect_type = 1;
+                    $redirect_type = 1;
                 elseif(!$is_languages_code)
-                $languages_code = DEFAULT_LANGUAGE;
+                    $languages_code = DEFAULT_LANGUAGE;
                 // a quick hack to redirect site.com/ru to site.com/ru/ to be consistent
                 elseif(strpos($current_page_url = $this->getUri(), "$languages_code/") === false){
                     $this->redirect($current_page_url.'/');
@@ -190,7 +204,7 @@ class Link {
             // we may have to convert back from alias
             if(array_key_exists($parts[0], $this->aliases))
                 $parts[0] = $this->aliases[$parts[0]];
-            
+
             // lets see if the page is in our list
             if(array_key_exists($parts[0], $this->pages)){
                 $ssu_get['main_page'] = $parts[0];
@@ -203,40 +217,40 @@ class Link {
                             $redirect_type = 1;
                             break;
                         }
-                    }                                    
+                    }
                     // found nothing?
                     if(!isset($ssu_get['main_page'])){
                         $ssu_get['main_page'] = $parts[0];
                         unset($parts[0]);
                     }
                 }
-    
+
                 // we want to make sure there is no extra main_page query left
                 if(($pos = array_search('main_page', $parts)) !== false){
                     unset($parts[$pos]);
                     unset($parts[$pos+1]);
                 }
             }
-            
+
             /*
-             * This is where we loop thru the query parts and put things into their places
-             * We need to do it this way because we want to keep the generated GET array in the correct order.
-             */
+            * This is where we loop thru the query parts and put things into their places
+            * We need to do it this way because we want to keep the generated GET array in the correct order.
+            */
             $parts = array_values($parts);
             $parts_count = count($parts);
-            
+
             for($counter = 0; $counter < $parts_count; $counter++){
                 $parser_encountered = false;
                 foreach (Plugin::get('settings')->get('riSsu.parsers') as $parser){
                     if(Plugin::get('riSsu.'.$parser)->identifyParameter($parts[$counter], $ssu_get) !== false){
                         $redirect_type = 1;
-                        $parser_encountered = true; 
+                        $parser_encountered = true;
                         break;
                     }
                 }
-                
+
                 if(!$parser_encountered)
-                $ssu_get[$parts[$counter]] = isset($parts[$counter+1]) ? $parts[++$counter] : '';
+                    $ssu_get[$parts[$counter]] = isset($parts[$counter+1]) ? $parts[++$counter] : '';
             }
 
             $this->rebuildENV($ssu_get, $catalog_dir);
@@ -263,17 +277,25 @@ class Link {
 
         // no need to include session id
         $session_name = zen_session_name();
-        if(isset($_get[$session_name])) unset($_get[$session_name]);
+        $session_flag = false;
+        if(isset($_get[$session_name])) {
+            $session_flag = true;
+            unset($_get[$session_name]);
+        }
 
         foreach($_get as $key => $value)
-        $params .= '&' . $key . '=' . $value;
+            $params .= '&' . $key . '=' . $value;
 
         $regenerated_link = $this->link($page, $params, $request_type, true, true, false, true, false);
 
         $current_url = trim($this->getPageBase(), '/') . $_request_uri;
-        
+
         // as long as we think the regenerated url fits in the current url, we are ok 
         if($regenerated_link != '' && strpos($current_url, $regenerated_link) != false){
+            $this->redirect($regenerated_link);
+        }
+
+        if($session_flag && $this->spider && Plugin::get('settings')->get('riSsu.session_block_spiders') ){
             $this->redirect($regenerated_link);
         }
     }
@@ -281,10 +303,10 @@ class Link {
     private function redirect($link){
         // Set POST form info / alpha testing
         if($_SERVER["REQUEST_METHOD"] == 'POST')
-        $_SESSION['ssu_post'] = $_POST;
-        
+            $_SESSION['ssu_post'] = $_POST;
+
         $response = new Response('', 301, array('Location' => $link));
-        $response->send();        
+        $response->send();
         exit();
     }
 
@@ -310,9 +332,9 @@ class Link {
     private function getPageBase() {
         global $request_type;
         if($request_type == 'SSL' && ENABLE_SSL == 'true')
-        $pageURL = HTTPS_SERVER;
+            $pageURL = HTTPS_SERVER;
         else
-        $pageURL = HTTP_SERVER;
+            $pageURL = HTTP_SERVER;
 
         return $pageURL;
     }
@@ -322,15 +344,15 @@ class Link {
      * Takes the same params as zencart zen_href_link function
      */
     public function link($page = '', $parameters = '', $connection = 'NONSSL', $add_session_id = true, $search_engine_safe = true, $static = false, $use_dir_ws_catalog = true, $browser_safe = true){
-        
+
         if(!Plugin::get('settings')->get('riSsu.status')) return false;
-        
+
         global $request_type, $session_started, $http_domain, $https_domain;
-        
+
         $this->current_page = $link = $sid = '';
 
         $languages_code = isset($_SESSION['languages_code']) ? $_SESSION['languages_code'] : DEFAULT_LANGUAGE;
-        
+
         // if this is anything other than index.php, dont ssu it
         if(strpos($page, '.php') !== false && strpos($page, 'index.php') === false) return false;
 
@@ -361,7 +383,7 @@ class Link {
 
             if($page == 'index' && strpos($parameters, 'cPath=') !== false)
                 $page = 'categories';
-                
+
             $this->current_page = $page;
             // if this page is our exclude list, let zen handle the job
             if (!array_key_exists($page, Plugin::get('settings')->get('riSsu.pages'))) return false;
@@ -370,7 +392,7 @@ class Link {
         }
 
         // Build session id        
-        if ( ($add_session_id == true) && ($session_started == true) && (SESSION_FORCE_COOKIE_USE == 'False') ) {
+        if ( !$this->spider && ($add_session_id == true) && ($session_started == true) && (SESSION_FORCE_COOKIE_USE == 'False') ) {
             if (defined('SID') && zen_not_null(SID)) {
                 $sid = SID;
             } elseif ( ($connection == 'SSL' && ENABLE_SSL == 'true') || ($connection == 'NONSSL') ) {
@@ -381,7 +403,7 @@ class Link {
         }
 
         if(empty($parameters['static']) && $page == 'index')
-        $page = '';
+            $page = '';
 
         // build the http://www.site.com
         $link = ($connection == 'SSL' && ENABLE_SSL == 'true') ? HTTPS_SERVER . DIR_WS_HTTPS_CATALOG : HTTP_SERVER . DIR_WS_CATALOG;
@@ -393,34 +415,34 @@ class Link {
         // append language code if:
         // multi lang is on AND we use default lang id OR We dont use default lang id but the current lang is not default
         if(Plugin::get('settings')->get('riSsu.multilang_status') &&
-        (!Plugin::get('settings')->get('riSsu.multilang_default_identifier') ||
-        (Plugin::get('settings')->get('riSsu.multilang_default_identifier') && $languages_code != DEFAULT_LANGUAGE)))
-        $link .= "/$languages_code";
+            (!Plugin::get('settings')->get('riSsu.multilang_default_identifier') ||
+                (Plugin::get('settings')->get('riSsu.multilang_default_identifier') && $languages_code != DEFAULT_LANGUAGE)))
+            $link .= "/$languages_code";
 
         if(!empty($page))
-        $link .= '/'.$page;
+            $link .= '/'.$page;
 
         if(!empty($parameters['static']))
-        $link .= '/'.$parameters['static'];
+            $link .= '/'.$parameters['static'];
 
         if(!empty($page) || !empty($parameters['static'])){
             if(!empty($extension))
-            $link .= $extension;
+                $link .= $extension;
             else
-            $link .= '/';
+                $link .= '/';
         }
         else
-        $link .= '/';
+            $link .= '/';
 
         // append sid
         if(!empty($sid))
-        if(empty($parameters['dynamic']))
-        $parameters['dynamic'] = $sid;
-        else
-        $parameters['dynamic'] .= '&'.$sid;
+            if(empty($parameters['dynamic']))
+                $parameters['dynamic'] = $sid;
+            else
+                $parameters['dynamic'] .= '&'.$sid;
 
         if(!empty($parameters['dynamic']))
-        $link .= '?'.$parameters['dynamic'];
+            $link .= '?'.$parameters['dynamic'];
 
         return $browser_safe ? str_replace('&', '&amp;', $link) : $link;
     }
@@ -449,9 +471,9 @@ class Link {
         // do we find this page in the parser mapping?
         if(($mapped_page = Plugin::get('settings')->get('riSsu.pages.' . $page)) != null){
             $options = $this->pages[$page];
-                        
+
             $params['dynamic'] = http_build_query(Plugin::get('riSsu.'.$options['parser'])->getDynamicQueryKeys($parameters));
-            
+
             $cache_filename = md5(Plugin::get('riSsu.'.$options['parser'])->getCacheKey($page, $parameters)).'_'.$languages_code;
 
             if(($cache = Plugin::get('riCache.Cache')->read('ssu/pc/' . $cache_filename, true)) !== false){
@@ -462,20 +484,20 @@ class Link {
             $params['static'] = implode('/', Plugin::get('riSsu.'.$options['parser'])->getStaticQueryKeys($parameters, $page, $languages_id, $languages_code));
 
             Plugin::get('riSsu.'.$options['parser'])->processPage($page, $mapped_page['alias']);
-                                    
+
             $extension = isset($mapped_page['extension']) ? $mapped_page['extension'] : Plugin::get('settings')->get('riSsu.file_extension');
 
             $set_cache = true;
         }
         else
             $params['dynamic'] = http_build_query($parameters);
-            
+
         // some alias stuffs
         if(Plugin::get('settings')->get('riSsu.alias_status')){
             if(!empty($params['static']))
-            Plugin::get('riSsu.Alias')->linkToAlias($params['static']);
+                Plugin::get('riSsu.Alias')->linkToAlias($params['static']);
             if(!empty($page))
-            Plugin::get('riSsu.Alias')->linkToAlias($page);
+                Plugin::get('riSsu.Alias')->linkToAlias($page);
         }
 
         if($set_cache){
@@ -496,9 +518,9 @@ class Link {
 
             $languages = $db->Execute($languages_query);
             if($languages->RecordCount() > 0)
-            $_SESSION['ssu_languages_code'][$languages_code] = $languages->fields['languages_id'];
+                $_SESSION['ssu_languages_code'][$languages_code] = $languages->fields['languages_id'];
             else
-            $_SESSION['ssu_languages_code'][$languages_code] = false;
+                $_SESSION['ssu_languages_code'][$languages_code] = false;
 
         }
         return $_SESSION['ssu_languages_code'][$languages_code];
--- riSsu.php
+++ riSsu.php
@@ -7,6 +7,13 @@ use plugins\riPlugin\Plugin;
 class RiSsu extends PluginCore{
     
     public function install(){
+        // create the list of spiders
+        $spiders = file(DIR_FS_CATALOG . DIR_WS_INCLUDES . 'spiders.txt');
+        foreach($spiders as $key => $spider){
+            $spiders[$key] = trim($spider);
+        }
+        Plugin::saveSettings('riSsu', array('spiders' => $spiders));
+
         return Plugin::get('riCore.DatabasePatch')->executeSqlFile(file(__DIR__ . '/install/sql/install.sql'));
         return true;
     }
