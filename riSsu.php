e76d3eebec519fe28a7dbfe8095938c6adcff909
yellow1912 <yellow1912@gmail.com>
2012-06-27 11:51:23 +0700

    change sql location

--- riSsu.php
+++ riSsu.php
@@ -7,12 +7,12 @@ use plugins\riPlugin\Plugin;
 class RiSsu extends PluginCore{
     
     public function install(){
-        return Plugin::get('riCore.DatabasePatch')->executeSqlFile(file(__DIR__ . '/sql/install.sql'));
+        return Plugin::get('riCore.DatabasePatch')->executeSqlFile(file(__DIR__ . '/install/sql/install.sql'));
         return true;
     }
     
     public function uninstall(){
-        return Plugin::get('riCore.DatabasePatch')->executeSqlFile(file(__DIR__ . '/sql/uninstall.sql'));
+        return Plugin::get('riCore.DatabasePatch')->executeSqlFile(file(__DIR__ . '/install/sql/uninstall.sql'));
         return true;
     }
 }
