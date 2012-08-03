1212ed7c08470da3d99021ac04897c1e45b51d04
yellow1912 <yellow1912@gmail.com>
2012-08-03 09:46:17 +0700

    minor update

--- riSsu.php
+++ riSsu.php
@@ -15,11 +15,9 @@ class RiSsu extends PluginCore{
         Plugin::saveSettings('riSsu', array('spiders' => $spiders));
 
         return Plugin::get('riCore.DatabasePatch')->executeSqlFile(file(__DIR__ . '/install/sql/install.sql'));
-        return true;
     }
     
     public function uninstall(){
         return Plugin::get('riCore.DatabasePatch')->executeSqlFile(file(__DIR__ . '/install/sql/uninstall.sql'));
-        return true;
     }
 }
